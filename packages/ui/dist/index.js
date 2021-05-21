61cfc7386d6bab2ba536b6973e81bf50ddde9405
alexandrehpiva <alexandrehpiva@gmail.com>
2021-05-21 15:53:56 -0300

    Add api package

--- packages/ui/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const diceroll_1 = require("@monorepo/diceroll");
-console.log(diceroll_1.roll('1d20'));
-//# sourceMappingURL=index.js.map
