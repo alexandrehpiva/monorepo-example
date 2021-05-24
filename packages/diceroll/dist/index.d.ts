9c0be589b062015af9841b24390660e8a0ca1f00
alexandrehpiva <alexandrehpiva@gmail.com>
2021-05-24 13:07:10 -0300

    Add config and delete unneeded files

--- packages/diceroll/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function roll(dice: string): string;
-//# sourceMappingURL=index.d.ts.map
--- packages/diceroll/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.roll = void 0;
-function roll(dice) {
-    return `I rolled a dice: ${dice}. Outcome grim`;
-}
-exports.roll = roll;
-//# sourceMappingURL=index.js.map
